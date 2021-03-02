24ba9558c30cd1cbc4103fdd45f39049ccdecc7f
Anthony Kizer <anthony.kizer88@gmail.com>
2021-03-01 23:01:39 -0800

    list approval process now works without bugs

--- imports/ui/components/List.js
+++ imports/ui/components/List.js
@@ -6,7 +6,11 @@ const List = ({ list, isOwner }) => {
         <div className="list">
             {isOwner && <AddUser listId={list._id} />}
 
-            <h3>{list.name || list._id} created by: {list.creatorName}</h3>{list.items && list.items.map((item, idx) => <p key={idx}>{item}
+
+            <h3>{list.name || list._id} created by: {list.creatorName}</h3>
+            {isOwner && <small><em>shared with: {list.acceptedShare && list.acceptedShare.length && list.acceptedShare.map(user => user) || "nobody"}</em></small>}
+
+            {list.items && list.items.map((item, idx) => <p key={idx}>{item}
             </p>)}
             <ItemForm listId={list._id} />
         </div>
